edf39074f58c6d6cfdd77ee02141cfe2b26ed0fd
Milena Andrade <81273891+milena-andrade@users.noreply.github.com>
2023-10-03 02:38:13 -0300

    Create Anonimo.cs

---
+++ Anonimo.cs
@@ -0,0 +1,4 @@
+String toAddress = 'milena.costandrade123456@gmail.com';
+String subject = 'Subject do email';
+String body = 'Email pelo apex';
+SendEmail.sendEmailMethod(toAddress, subject, body);
